b380ecb5b9c729c56553e815a8fa41d1fb709210
Raja Vyshnavi Sriramoju <rsriramoju@ucdavis.edu>
2019-03-05 19:14:05 -0800

    Added data extraction server code

--- dashboards/solarplus-ui/server/app.py
+++ dashboards/solarplus-ui/server/app.py
@@ -61,13 +61,38 @@ def cieeData():
     ciee = pd.read_csv("./sample_data/ciee.csv")
     ciee.columns = ['TimeStamp', 'ciee', 's0', 's1', 's2', 's3']
 
-    return ciee.to_json(orient='records')
-    
-@app.route('/avenalData')
-@crossdomain(origin="*")
-def avenalData():
-    avenal = pd.read_csv("./sample_data/avenal-public-works-yard.csv")
-    avenal.columns = ['TimeStamp', 'avenal-public-works-yard', 's0']
+    #limiting to 20 entries like prev demo
+    ciee = ciee[:20]
 
-    return avenal.to_json(orient='records')
+    return ciee.to_json(orient='records')
+
+
+@app.route('/cieeData/<startDate>/<endDate>')
+@crossdomain(origin="*")
+def extractData(startDate, endDate):
+    cieeDF = pd.read_csv("./sample_data/ciee.csv")
+    cieeDF.columns = ['TimeStamp', 'ciee', 's0', 's1', 's2', 's3']
+
+    # check for validity of range of dates
+    startYear,startMonth,startDay=[int(x) for x in startDate.split('-')]
+    endYear,endMonth,endDay=[int(x) for x in endDate.split('-')]
+
+    if(datetime.datetime(startYear,startMonth,startDay) > datetime.datetime(endYear,endMonth,endDay)):
+        print ('Wrong range of dates given. Start Date = ' ,startDate, "; End Date = ", endDate)
+        return 'Incorrect Range of dates'
+
+    # This gets all the entries of the specific start date and end date
+    startDateEntries = cieeDF[cieeDF['TimeStamp'].str.contains(startDate)]
+    endDateEntries = cieeDF[cieeDF['TimeStamp'].str.contains(endDate)]
+
+    # finding the first index of start date entries and last index of the end date entries
+    # so that we can get the range of indices for the data in the specified timeframe
+    startDateIndex = startDateEntries.index[0]
+    endDateIndex = endDateEntries.index[-1]
+
+    #fetching data in the specific timeframe
+    dataInRange = cieeDF[startDateIndex:(endDateIndex+1)]
+    #print(dataInRange)
+
+    return dataInRange.to_json(orient = 'records')
   
