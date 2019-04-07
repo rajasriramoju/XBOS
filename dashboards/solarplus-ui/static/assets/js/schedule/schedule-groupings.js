d6ce1fff5ee9d6c9ef6cf9d72ecd44d8a1e04a07
fuzionllf <fuzion117@gmail.com>
2019-04-06 22:01:51 -0700

    added buttons plus functionality

--- dashboards/solarplus-ui/static/assets/js/schedule/schedule-groupings.js
+++ dashboards/solarplus-ui/static/assets/js/schedule/schedule-groupings.js
@@ -58,20 +58,20 @@ $(document).ready(function() {
 	// 	console.log(obj);
 	// });
 	
-	$("#save-modes").click(function() {
+	$("#save-therm").click(function() {
 		//var obj = new Object();
 		modes = [];
-		$(".mode-card").each(function(i) {
+		$(".thermostat-card").each(function(i) {
 			var m = new Object();
 			m.id = i;
 			var inputs = $(this).find("input");
 			m.name = inputs[0].value;
 			m.heating = inputs[1].value;
 			m.cooling = inputs[2].value;
-			m.enabled = $(inputs[3]).prop("checked");
+
 			modes.push(m);
 		});
-		M.toast({html: 'Current modes successfully updated.', classes:"rounded", displayLength: 2000});
+		M.toast({html: 'Current setpoints successfully updated.', classes:"rounded", displayLength: 2000});
 
 		console.log(modes);
 
@@ -105,7 +105,60 @@ $(document).ready(function() {
 		var hiddenElement = document.createElement('a');
 		hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(result);
 		hiddenElement.target = '_blank';
-		hiddenElement.download = 'currentModes.csv';
+		hiddenElement.download = 'thermostatSetpoints.csv';
+		hiddenElement.click();
+
+
+		// console.log("name" + modes[0].name);
+	});
+	$("#save-sp").click(function() {
+		//var obj = new Object();
+		modes = [];
+		$(".refrigerator-freezer-card").each(function(i) {
+			var m = new Object();
+			m.id = i;
+			var inputs = $(this).find("input");
+			m.name = inputs[0].value;
+			m.heating = inputs[1].value;
+			m.cooling = inputs[2].value;
+
+			modes.push(m);
+		});
+		M.toast({html: 'Current setpoints successfully updated.', classes:"rounded", displayLength: 2000});
+
+		console.log(modes);
+
+		var result, ctr, keys, columnDelimiter, lineDelimiter, data, csv, eCSV;
+		data = modes;
+        columnDelimiter =  ',';
+        lineDelimiter =  '\n';
+
+        keys = Object.keys(data[0]);
+
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        data.forEach(function(item) {
+            ctr = 0;
+            keys.forEach(function(key) {
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key];
+                ctr++;
+            });
+            result += lineDelimiter;
+		});
+		
+		csv = 'data:text/csv;charset=utf-8,' + result;
+		eCSV = encodeURI(csv);
+
+		//console.log(eCSV);
+
+		var hiddenElement = document.createElement('a');
+		hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(result);
+		hiddenElement.target = '_blank';
+		hiddenElement.download = 'Refrigerator-Freezer-Setpoints.csv';
 		hiddenElement.click();
 
 
