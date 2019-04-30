ba0539dc2a1d82862bde021c0e161b2b71f61615
Raja Vyshnavi Sriramoju <rsriramoju@ucdavis.edu>
2019-04-29 20:55:07 -0700

    restructured files and WIP for server side

---
+++ dashboards/solarplus-ui/project/main.py
@@ -0,0 +1,141 @@
+# main.py
+
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from flask import Flask, request, send_from_directory
+from flask import make_response, request, current_app
+from flask import jsonify, redirect, url_for
+from datetime import timedelta
+from functools import update_wrapper 
+import pandas as pd 
+import datetime
+
+main = Blueprint('main', __name__)
+
+@main.route('/index')
+@login_required
+def index():
+    return render_template('index.html')
+
+# Flask boilerplate, 
+# configure CORS to make HTTP requests from javascript
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, str):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, str):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
+@main.route('/<path:path>')
+def static_proxy(path):
+    return send_static_file(path)
+
+
+@main.route('/cieeData/<startDate>/<endDate>')
+@crossdomain(origin="*")
+def extractData(startDate, endDate):
+    cieeDF = pd.read_csv("./sample_data/ciee.csv")
+    cieeDF.columns = ['TimeStamp', 'ciee', 's0', 's1', 's2', 's3']
+
+    # check for validity of range of dates
+    startYear,startMonth,startDay=[int(x) for x in startDate.split('-')]
+    endYear,endMonth,endDay=[int(x) for x in endDate.split('-')]
+
+    if(datetime.datetime(startYear,startMonth,startDay) > datetime.datetime(endYear,endMonth,endDay)):
+        print ('Wrong range of dates given. Start Date = ' ,startDate, "; End Date = ", endDate)
+        return 'Incorrect Range of dates'
+
+    # This gets all the entries of the specific start date and end date
+    startDateEntries = cieeDF[cieeDF['TimeStamp'].str.contains(startDate)]
+    endDateEntries = cieeDF[cieeDF['TimeStamp'].str.contains(endDate)]
+
+    # finding the first index of start date entries and last index of the end date entries
+    # so that we can get the range of indices for the data in the specified timeframe
+    startDateIndex = startDateEntries.index[0]
+    endDateIndex = endDateEntries.index[-1]
+
+    #fetching data in the specific timeframe
+    dataInRange = cieeDF[startDateIndex:(endDateIndex+1)]
+    #print(dataInRange)
+
+    return dataInRange.to_json(orient = 'records')
+
+
+# This function takes in a file name, start and end date with the two features that 
+# the user wants plotted on the graph
+@main.route('/<filename>/<startDate>/<endDate>/<feature1>/<feature2>')
+@crossdomain(origin="*")
+def extractData_plotTwoQueries(filename, startDate, endDate, feature1, feature2):
+    
+    filePathString = "./solarplus_sample_data/" + filename+".csv"
+    print(filePathString)
+    readDF = pd.read_csv(filePathString)
+    
+    '''The names the columns of the data frame using the first row info - assumes that column names 
+     are entered correctly in the csv files - which is why the column names are not renamed in this
+     function. '''
+    
+    # check for validity of range of dates
+    startYear,startMonth,startDay=[int(x) for x in startDate.split('-')]
+    endYear,endMonth,endDay=[int(x) for x in endDate.split('-')]
+
+    if(datetime.datetime(startYear,startMonth,startDay) > datetime.datetime(endYear,endMonth,endDay)):
+        print ('Wrong range of dates given. Start Date = ' ,startDate, "; End Date = ", endDate)
+        return 'Incorrect Range of dates'
+
+    
+    # This gets all the entries of the specific start date and end date
+    startDateEntries = readDF[readDF['Time'].str.contains(startDate)]
+    endDateEntries = readDF[readDF['Time'].str.contains(endDate)]
+
+    # finding the first index of start date entries and last index of the end date entries
+    # so that we can get the range of indices for the data in the specified timeframe
+    startDateIndex = startDateEntries.index[0]
+    endDateIndex = endDateEntries.index[-1]
+
+    #fetching data in the specific timeframe
+    dataInRange = readDF[startDateIndex:(endDateIndex+1)]
+
+    dataInRange = dataInRange.loc[:,['Time', feature1, feature2]]
+
+    return dataInRange.to_json(orient = 'records')
+
+
+
+
+
---
+++ dashboards/solarplus-ui/project/models.py
@@ -0,0 +1,12 @@
+# models.py
+
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    firstName = db.Column(db.String(100))
+    lastName = db.Column(db.String(1000))
