286c9ae89196f0628fc58b92a75a4c891b3d7143
Raja Vyshnavi Sriramoju <rsriramoju@ucdavis.edu>
2019-05-13 15:53:48 -0700

    Graphs on 4 separate containers - WIP - issue with onload func

--- dashboards/solarplus-ui/static/assets/js/index/dashboard-graphs.js
+++ dashboards/solarplus-ui/static/assets/js/index/dashboard-graphs.js
@@ -1,18 +1,18 @@
 /*dashboard-graphs.js*/
 $(document).ready(function() {
 
-        function consumption(value){
-        return (value);
+    function consumption(value){
+       return (value);
     }
 
-    function renderChart(buildingVals, dates) {
-        var highChart = new Highcharts.chart('Chart1',{
+    function renderChart1(yVals, yValsTitle, dates) {
+        var highChart = new Highcharts.chart('chart1',{
             chart: {
                 type: 'line',
                 zoomType: "x"
             },
             title: {
-                text: 'Total Power Consumption'
+                text: yValsTitle
             },
             tooltip : {
                 valueSuffix: '\xB0C'
@@ -31,7 +31,39 @@ $(document).ready(function() {
             series: [
             {
                 name: 'Building consumption',
-                data: buildingVals
+                data: yVals
+            }]
+
+        });
+    }
+
+    function renderChart2(yVals, yValsTitle, dates) {
+        var highChart = new Highcharts.chart('chart2',{
+            chart: {
+                type: 'line',
+                zoomType: "x"
+            },
+            title: {
+                text: yValsTitle
+            },
+            tooltip : {
+                valueSuffix: '\xB0C'
+            },
+            xAxis: {
+                categories: dates
+            },
+            yAxis: {
+                title: {
+                    text: 'Power generated'
+                },
+                plotLines: [{
+                    value:0
+                }]
+            },
+            series: [
+            {
+                name: 'PV Power Generated',
+                data: yVals
             }]
 
         });
@@ -44,8 +76,14 @@ $(document).ready(function() {
         return Httpreq.responseText;          
     }
     
-        function init() {
+    function loadAllGraphs()
+    {
+        graphDataCollection_Chart1();
+    }
+        function graphDataCollection_Chart1() {
 
+            // will have to include code here about fetching data from the 
+            // selected features for the specific features on dashboard
             const uri_chart1 = `http://localhost:5000/dashboard/Building`;        
             var res_chart1 = JSON.parse(Get(uri_chart1)); 
             console.log(res_chart1);
@@ -68,9 +106,36 @@ $(document).ready(function() {
             
             console.log(labels);
             console.log(buildingValsVals);
-            //console.log(feature2Vals)
-            renderChart(buildingVals, labels);
+            renderChart(buildingVals, 'Total Power Consumption Values', labels);
             
         }
+        /*
+        function graphDataCollection_Chart2() {
+            // will have to include code here about fetching data from the 
+            // selected features for the specific features on dashboard
+            const uri_chart1 = `http://localhost:5000/dashboard/PVPowerGenData`;        
+            var res_chart1 = JSON.parse(Get(uri_chart1)); 
+            console.log(res_chart1);
+            
+            var powerGenVals = [];
+            //var feature2Vals = []
+            var labels = [];
+            
+            
+            for(let i = 0; i < res_chart1.size; i++){
 
+                let singleElement = res_chart1[i];
+
+                for(let prop in singleElement){
+                    if(prop == 'PVPower_kW')
+                        powerGenVals.push(singleElement[prop]);           
+                }
+                labels.push(res[i].Time);
+            }        
+            console.log(labels);
+            console.log(powerGenVals);
+            renderChart(powerGenVals, labels);
+            
+        }*/
+    );
 });
