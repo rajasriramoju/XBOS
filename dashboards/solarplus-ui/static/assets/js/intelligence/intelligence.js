41c99210411e3136f58ad7fca599d5fa83302048
fuzionllf <fuzion117@gmail.com>
2019-05-19 01:11:30 -0700

    intelligence settings saves on different pages now

--- dashboards/solarplus-ui/static/assets/js/intelligence/intelligence.js
+++ dashboards/solarplus-ui/static/assets/js/intelligence/intelligence.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
+    var intelligenceText = sessionStorage.getItem('text');
+    var intelligenceColor = sessionStorage.getItem('color');
+    if(intelligenceText == 'on') {
+        console.log('inside on')
+        document.getElementById("check").checked = true;
+        document.getElementById("intelligence-control-on-off").innerText = "Intelligent Control On"
+        document.getElementById("intelligent-control").style.backgroundColor = "green"
+    } 
+    if(intelligenceText == 'off'){
+        console.log('inside')
+        document.getElementById("check").checked = false;
+        document.getElementById("intelligence-control-on-off").innerText = "Intelligent Control Off"
+        document.getElementById("intelligent-control").style.backgroundColor = "red"
+    }
     $("input").change(function(){
         if($(this).is(":checked")){
             console.log("Is checked");
+            sessionStorage.setItem('text','on');
+            sessionStorage.setItem('color','on');
             document.getElementById("intelligence-control-on-off").innerText = "Intelligent Control On"
             document.getElementById("intelligent-control").style.backgroundColor = "green"
         }
         else{
             console.log("Is Not Checked");
+            sessionStorage.setItem('text','off');
+            sessionStorage.setItem('color','off');
             document.getElementById("intelligence-control-on-off").innerText = "Intelligent Control Off"
             document.getElementById("intelligent-control").style.backgroundColor = "red"
         }
